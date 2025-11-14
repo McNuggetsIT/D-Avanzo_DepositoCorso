e7a9d3fb4618ab2e9e84d3de4ba9004a320249e8
McNuggetsIT <davanzogiuseppe1@outlook.it>
2025-11-14 16:25:31 +0100

    Update Es_Riassuntivo.py
    update.

--- Python Corso/Venerdi 14-11/Esercizio_difficile/Es_Riassuntivo.py
+++ Python Corso/Venerdi 14-11/Esercizio_difficile/Es_Riassuntivo.py
@@ -19,6 +19,11 @@ persone = [
     ("Chiara", "Romano", 23)
 ]
 
+def stampa_persone(persone):
+    for nome, cognome, eta in persone:
+        print(f"Nome: {nome:<10} Cognome: {cognome:<10} Età: {eta}")
+
+
 def find_person(nome_cercato):
     for persona in persone:
         if persona[0].lower() == nome_cercato.lower():
@@ -36,14 +41,37 @@ def add_person():
         print("Persona già esistente")
     else:
         persone.append(new_person)
-    print(persone)
+    print(persone)        
     
-def delete_person(nome_cercato):    
-        for persona in persone:
-            if persona[0].lower() == nome_cercato():
-                persona.remove(nome_cercato)
-        return "Persona non trovata"        
 def modify_person():
+    stampa_persone(persone)
+    persona = input("Scegli chi modificare").lower()
+    if persona in persone:
+        scelta = input("Cosa vuoi modificare? A: Nome B: Cognome : C Eta").lower()
+        
+        if scelta == "a":
+            new_name = input("Scegli nuovo nome: ").lower()
+            persone[new_name] = persone.pop(persona)
+            print(f"Nome '{persona}' cambiato in '{new_name}'")
+        if scelta == "b":
+            new_surname = input("Scegli nuovo cognome: ").lower()
+            persone[new_surname] = persone.pop(persona)
+            print(f"Cognome '{persona}' cambiato in '{new_surname}'")
+        if scelta == "c":
+            new_age = int(input("Scegli la nuova età: "))
+            persone[new_age] = persone.pop(persona)
+            print(f"Nome '{persona}' cambiato in '{new_age}'")
+        else:
+            print("Persona non trovata")    
+    
+def delete_person(nome_cercato):
+    stampa_persone(persone)
+    for persona in persone:
+        if persona[0].lower() == nome_cercato():
+            persone.remove(nome_cercato)
+            print(f"{nome_cercato} eliminato con successo")
+            return
+    return "Persona non trovata"
     
 print("Fai una scelta:  \n1: Cerca persona  \n2: Aggiungi una persona \n3: Modifica una persona  \n5: Elimina una persona")
 
@@ -55,9 +83,10 @@ match scelta :
     case "2":
         add_person()
     case "3":
-        pass
+        modify_person()
     case "4":
-        pass
+        nome = input("Chi vuoi eliminare?")
+        delete_person(nome)
     case _:
         print("ERRORE")
     
