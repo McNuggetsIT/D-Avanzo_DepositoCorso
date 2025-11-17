6067c99fa8acb0b8f1b5ea0d568f70e0e7108e16
McNuggetsIT <davanzogiuseppe1@outlook.it>
2025-11-17 11:16:49 +0100

    Aggiunta di note

--- Python Corso/11_17_Lunedi/Esercizi_Objects_1.py
+++ Python Corso/11_17_Lunedi/Esercizi_Objects_1.py
@@ -4,12 +4,13 @@
 # - Un metodo muovi(dx, dy): modifica le coordinate aggiungendo dx e dy.
 # - Un metodo distanza_da_origine(): restituisce la distanza dal punto (0,0).
 
+#crazione classe
 class Punto:
-    
+    #creazione del costruttore
     def __init__(self, x, y):
         self.x= x
         self.y= y
-        
+    #istanziamento del metodo muovi
     def muovi(self):
        dx_sx = input("Dove vuoi spostarlo dx-sx?").lower()
        
@@ -19,11 +20,11 @@ class Punto:
        else:
            qnt = int(input("di quante celle?"))
            self.y += qnt
-           
+    #metodo stampa 
     def distanza_da_origine(self):
         return f"La distanza dal punto 0.0 è di:{self.x} sull'asse X e {self.y} sull'Asse Y"
     
-
+#utilizzo della classe
 p1 = Punto(0,0)
 p1.muovi()
 print(p1.distanza_da_origine())
--- Python Corso/11_17_Lunedi/Esercizi_Objects_2.py
+++ Python Corso/11_17_Lunedi/Esercizi_Objects_2.py
@@ -4,15 +4,17 @@
 # - Un metodo descrizione(): restituisce una stringa del tipo:
 #   "Il libro 'titolo' è stato scritto da 'autore' e ha 'pagine' pagine."
 
+#creazione della classe
 class Libro:
-    
+    #istanziamento del costruttore
     def __init__(self,titolo,autore,pagine):
         self.titolo = titolo
         self.autore = autore
         self.pagine = pagine
-    
+    #creazione metodo descrizione
     def descrizione(self):
         return f"Il libro{self.titolo} è stato scritto da {self.autore} e ha {self.pagine} pagine"
-    
+
+#utilizzo della classe
 libro1 = Libro("Divina Commedia", "Dante", 350)
 print(libro1.descrizione())
